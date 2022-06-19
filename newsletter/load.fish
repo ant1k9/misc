9ac78602660c36b3d8c113c884b214a2f8c63a85
ant1k9 <ant1k9@protonmail.com>
2022-06-19 14:24:15 +0300

    feat: remove query params from links for newsletter

--- newsletter/load.fish
+++ newsletter/load.fish
@@ -3,6 +3,7 @@
 for i in (seq (jq ". | length" < "$argv[1]"))
     set -l i (math $i - 1)
     set -l href (jq ".[$i].href" < "$argv[1]")
+    set -l href (string replace -r '\?[^"]*' '' "$href")
     set -l title (jq ".[$i].title" < "$argv[1]")
     sqlite3 "links.sqlite3" \
         "INSERT INTO links (href, title, is_finished) VALUES ('$href', '$title', 0)" || true

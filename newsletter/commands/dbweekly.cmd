65fbf7043cf4334600dd13b148f9b58f9dce7132
ant1k9 <ant1k9@protonmail.com>
2022-10-28 03:16:48 +0400

    feat: remove outdated sources for newsletter

--- newsletter/commands/dbweekly.cmd
+++
@@ -1,3 +0,0 @@
-curl -L "https://dbweekly.com/issues/" \
-    | pup 'td p span a json{}' \
-    | jq 'map(({"href": .href, "title": .text})) | map(select(.title != null))' > "$LINKS_FILE"
--- newsletter/commands/rusttimes.cmd
+++
@@ -1,6 +0,0 @@
-curl -L "https://www.rusttimes.com/" \
-    -H 'authority: www.rusttimes.com' \
-    -H 'accept-language: en-US,en;q=0.9' \
-    --compressed \
-    | pup 'div#articles div.articles-content json{}' \
-    | jq 'map({"title": .children[0].text, "href": .children[3].children[0].children[0].children[0].children[0].href})' > "$LINKS_FILE"

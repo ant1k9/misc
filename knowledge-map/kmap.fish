fe4dfbaf72e618b2a0139d701f87b49e5cf34a75
ant1k9 <ant1k9@protonmail.com>
2022-11-11 19:26:54 +0400

    feat: add option for kmap to parse HTML files

--- knowledge-map/kmap.fish
+++ knowledge-map/kmap.fish
@@ -33,13 +33,45 @@ function _kmap_get_repo
     rm "$TMP_FILE"
 end
 
+function _kmap_get_html
+    set -l SITE "$argv[1]"
+    set -g TMP_FILE (mktemp)
+
+    curl "$SITE" | pup 'h1 json{}' > "$TMP_FILE"
+
+    function _get_jq_text_attr
+        jq "$argv[1] | ..|objects|.text//empty" "$TMP_FILE" | tr -d '"\n'
+    end
+
+    echo "["(_get_jq_text_attr '.[0]')"]($SITE)"
+    echo
+    if test (cat "$TMP_FILE" | jq 'length') -eq 2
+        echo "*Description*: "(_get_jq_text_attr '.[1]')
+    else
+        echo "*Description*: "(_get_jq_text_attr '.[0]')
+    end
+    echo
+    echo "*Labels*: # "
+
+    rm "$TMP_FILE"
+end
+
+function _kmap_get_resource
+    set -l RESOURCE "$argv[1]"
+    if string match -r "https://github.com.*" "$RESOURCE" >/dev/null
+        _kmap_get_repo "$RESOURCE"
+    else
+        _kmap_get_html "$RESOURCE"
+    end
+end
+
 function _kmap_search
     open "$MY_KNOWLEDGE_MAP_HOST/search?q=$argv[1]"
 end
 
 # main
 if test "$argv[1]" = "get"
-    _kmap_get_repo "$argv[2]"
+    _kmap_get_resource "$argv[2]"
 else if test "$argv[1]" = "search"
     _kmap_search "$argv[2]"
 else

76072bfcaf6575c24ee72843f6b2a5666c8ad9f2
ant1k9 <ant1k9@protonmail.com>
2022-11-12 03:10:55 +0400

    feat: receive dropbox token from db-bacup-token env

--- dropbox/load-from-dropbox.sh
+++ dropbox/load-from-dropbox.sh
@@ -3,6 +3,7 @@ set -euo pipefail
 
 PAYLOAD_FILE="$1"
 DROPBOX_PATH=$DROPBOX_DIRECTORY/$(basename "$PAYLOAD_FILE")
+: ${DROPBOX_TOKEN:=$DB_BACKUP_TOKEN}
 
 curl -X POST https://content.dropboxapi.com/2/files/download \
     -H "Authorization: Bearer ${DROPBOX_TOKEN}" \

40d8c7b92e6a27f65f66dd889cef63b183da58da
ant1k9 <ant1k9@protonmail.com>
2022-11-13 02:44:58 +0400

    feat: change list command for misc

--- misc/misc.fish
+++ misc/misc.fish
@@ -3,7 +3,7 @@
 set INSTALL_DIR # TODO
 
 if test "$argv[1]" = "list"
-    grep -- '- ' "$INSTALL_DIR/README.md"
+    grep -- '- ' "$INSTALL_DIR/README.md" | sed 's/^-//'
 else if test "$argv[1]" = "update"
     fish -c "cd $INSTALL_DIR; and git pull"
 else if test "$argv[1]" = "install"
@@ -11,5 +11,5 @@ else if test "$argv[1]" = "install"
 else if test "$argv[1]" = "_list"
     fish -c "
         cd $INSTALL_DIR;
-        find . -maxdepth 1 -not -name '.*' -and -not -name 'misc' -type d | tr -d './'"
+        find . -maxdepth 1 -not -name '.*' -type d | tr -d './'"
 end

69e907b72d80743b195925e293d98e526f8ed950
ant1k9 <ant1k9@protonmail.com>
2023-11-06 12:20:21 +0300

    fix: replaces pipes with spaces in newsletter titles

--- newsletter/load.fish
+++ newsletter/load.fish
@@ -4,7 +4,7 @@ for i in (seq (jq ". | length" < "$argv[1]"))
     set -l i (math $i - 1)
     set -l href (jq ".[$i].href" < "$argv[1]")
     set -l href (string replace -r '\?[^"]*' '' "$href")
-    set -l title (jq ".[$i].title" < "$argv[1]")
+    set -l title (jq ".[$i].title" < "$argv[1]" | tr '|' ' ')
     sqlite3 "links.sqlite3" \
         "INSERT INTO links (href, title, is_finished) VALUES ('$href', '$title', 0)" || true
 end
